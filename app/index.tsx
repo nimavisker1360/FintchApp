66cc37499007612dd5e539ab8547e22e49d0b94d
nimavisker1360 <nimavisker@gmail.com>
2025-03-21 20:13:06 +0300

    fintch app

---
+++ app/index.tsx
@@ -0,0 +1,86 @@
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useAssets } from "expo-asset";
+import { ResizeMode, Video } from "expo-av";
+import { Link } from "expo-router";
+import { useRef, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Page = () => {
+  const videoRef = useRef(null);
+  const [isReady, setIsReady] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={videoRef}
+        resizeMode={ResizeMode.COVER}
+        isMuted
+        isLooping
+        shouldPlay={isReady}
+        source={require("@/assets/videos/intro.mp4")}
+        style={styles.video}
+        onLoad={() => setIsReady(true)}
+        useNativeControls={false}
+      />
+
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Link
+          href={"/login"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.dark },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+        <Link
+          href={"/signup"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: "#fff" },
+          ]}
+          asChild
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
+  },
+});
+export default Page;
