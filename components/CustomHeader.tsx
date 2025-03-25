bfe0aace83f69c7b7eee6f66de881305191bb9fe
nimavisker1360 <nimavisker@gmail.com>
2025-03-25 09:25:46 +0300

    Add CustomHeader

---
+++ components/CustomHeader.tsx
@@ -0,0 +1,95 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { BlurView } from "expo-blur";
+import Colors from "@/constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+
+const CustomHeader = () => {
+  const { top } = useSafeAreaInsets();
+  return (
+    <BlurView intensity={100} tint={"extraLight"} style={{ paddingTop: top }}>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.roundBtn}>
+          <Text style={{ color: "#fff", fontWeight: "500", fontSize: 16 }}>
+            SG
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.searchSection}>
+          <Ionicons
+            style={styles.searchIcon}
+            name="search"
+            size={20}
+            color={Colors.dark}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Search"
+            placeholderTextColor={Colors.dark}
+          />
+        </View>
+
+        <View style={styles.circle}>
+          <Ionicons name={"stats-chart"} size={20} color={Colors.dark} />
+        </View>
+        <View style={styles.circle}>
+          <Ionicons name={"card"} size={20} color={Colors.dark} />
+        </View>
+      </View>
+    </BlurView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    gap: 10,
+    backgroundColor: "transparent",
+    paddingHorizontal: 20,
+  },
+  roundBtn: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.gray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchSection: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: Colors.lightGray,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    color: Colors.dark,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default CustomHeader;
